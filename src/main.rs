a867fec58dbe437cb0f9f240766b68e7c3731296
Kharif <rydenfalk.service@gmail.com>
2024-12-30 21:59:53 +0100

    sync

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,9 @@ const APP_NAME: &str = "test window";
 fn main() -> Result<(), Box<dyn Error>> {
     SimpleLogger::new().init()?;
 
-    System::new(APP_NAME)?.run((), |run, ui, _| ui.show_demo_window(run))?;
+    let system = System::new(APP_NAME)?;
+    // system.imgui.io_mut().config_flags |= imgui::ConfigFlags::DOCKING_ENABLE;
+    system.run((), |run, ui, _| ui.show_demo_window(run))?;
 
     Ok(())
 }
